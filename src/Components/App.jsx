0136dfb86f4ca27c02a40b05c6c19370b1d5fa1c
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-08-03 13:10:14 +0300

    add style header

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Header/Header";
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <Header />
     </div>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,23 @@
+import { ReactComponent as Logo } from "../../Shared/images/Logo.svg";
+
 import s from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <nav className={s.Navigation}>
-      <ul className={s.listNav}>
-        <li className={s.listNavItem}>ГЛАВНАЯ</li>
-        <li className={s.listNavItem}>ГАЛЕРЕЯ</li>
-        <li className={s.listNavItem}>ПРОЕКТЫ</li>
-        <li className={s.listNavItem}>СЕРТИФИКАТЫ</li>
-        <li className={s.listNavItem}>КОНТАКТЫ</li>
-      </ul>
-    </nav>
+    <header className={s.header}>
+      <div className="container">
+        <nav className={s.Navigation}>
+          <Logo className={s.logo} />
+          <ul className={s.listNav}>
+            <li className={s.listNavItem}>ГЛАВНАЯ</li>
+            <li className={s.listNavItem}>ГАЛЕРЕЯ</li>
+            <li className={s.listNavItem}>ПРОЕКТЫ</li>
+            <li className={s.listNavItem}>СЕРТИФИКАТЫ</li>
+            <li className={s.listNavItem}>КОНТАКТЫ</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
