71deac41a0853edc43b9f3adda6420d2457f48eb
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-08-10 17:13:48 +0300

    add img and button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,36 @@
 import { ReactComponent as ArrowLeft } from "../../Shared/images/arrow_left.svg";
+import { ReactComponent as ArrowRight } from "../../Shared/images/arow_right.svg";
+import House from "../../Shared/images/Hero_image.png";
 import s from "./Hero.module.scss";
 
 const Hero = () => {
   return (
     <section className={s.hero}>
       <div className="container">
-        <h1 className={s.title}>
-          Project <span>home</span>
-        </h1>
-        <div className={s.arrowContainer}>
-          <div className={s.arrow}>
-            <ArrowLeft className={s.arrowLeft} />
+        <div className={s.heroContainer}>
+          <div className={s.descriptionHero}>
+            <h1 className={s.title}>
+              Project <span>home</span>
+            </h1>
+            <div className={s.arrowContainer}>
+              <div className={s.arrow}>
+                <ArrowLeft className={s.arrowLeft} />
+              </div>
+              <div className={s.arrow}>
+                <ArrowRight className={s.arrowLeft} />
+              </div>
+            </div>
+            <div className={s.numberContainer}>
+              <p className={s.numberOne}>01</p>
+              <p className={s.numberTwo}>02</p>
+            </div>
+          </div>
+          <div className={s.imgContainer}>
+            <img src={House} alt="house" className={s.titleImage} />
+            <button className={s.buttonRight}>
+              ВЗГЛЯНУТЬ
+              <ArrowRight className={s.buttonArrowRight} />
+            </button>
           </div>
         </div>
       </div>
